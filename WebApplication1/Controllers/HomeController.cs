3a2f6e8828cc24fd18408a99a0d0222d1ce081fe
MaximVir <dylanrchance@gmail.com>
2018-04-10 10:29:47 -0500

    added gender

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -29,7 +29,7 @@ namespace WebApplication1.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Great Contact Page";
 
             return View();
         }
